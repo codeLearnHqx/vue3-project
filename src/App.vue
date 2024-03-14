d3dda9e759093f41b12ce4814274bbef8dae8470
hqx <751156604@qq.com>
2024-03-15 01:22:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <h1>App根组件</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const str = '我爱祖国'
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
